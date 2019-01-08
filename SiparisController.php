94223cf1c1762c8cd83a3af0270425527beaf06f
Beste ÖZALP <31745124+besteozalp@users.noreply.github.com>
2019-01-08 20:17:21 +0300

    Add files via upload

---
+++ SiparisController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class SiparisController extends Controller
+{
+    public function index()
+    {
+        $siparis=DB::select("SELECT s.*,u.name as uyeadsoy FROM siparis s,users u WHERE u.Id=s.user_id AND durum='Yeni'");
+        return view("admin.siparis_yeni",compact('siparis'));
+    }
+    public function siparisler($durum)
+    {
+
+        $siparis=DB::select("SELECT s.*,u.name as uyeadsoy FROM siparis s,users u WHERE u.Id=s.user_id AND durum='$durum'");
+        return view("admin.siparisler",compact('siparis','durum'));
+    }
+
+    public function siparis_detay($id){
+        $siparis=DB::select('SELECT * FROM siparis WHERE Id=?',[$id]);
+        $urunler=DB::select('SELECT s.*,u.resim as resim FROM siparis_detay s ,urunler u WHERE s.urun_id=u.ID AND s.siparis_id=?',[$id]);
+        return view('admin.siparis_detay',compact('siparis','urunler'));
+    }
+    public function siparis_update(Request $request, $id)
+    {
+        DB::table('siparis')
+            ->where('Id',$id)
+            ->update(['aciklama'=>$request->aciklama,
+                      'durum'=>$request->durum
+            ]);
+
+        return redirect('admin/siparis_detay/'.$id)->with('success','Sipariş Güncellendi');
+    }
+}
